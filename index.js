11d4692d99c26b3d33817edea0f9580bf9005a93
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-11-05 10:52:35 +0700

    membuat endpoint delete

--- index.js
+++ index.js
@@ -56,3 +56,17 @@ app.put('/hotel/:id',  async (req,  res) => {
         res.send({ message: error.message });
     }
 });
+
+app.delete('/hotel/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const hotel = await db.hotel.findByPk(id);
+        if (!hotel) {
+            return res.status(404).send({ message: 'Hotel not found' });
+        }
+        await hotel.destroy();
+        res.send({ message: 'Hotel berhasil dihapus' });
+    } catch (error) {
+        res.status(500).send(err)
+    }
+});
